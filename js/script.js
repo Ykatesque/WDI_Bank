fdacdd36c333fc09bdca55a0d42cf2403230fd01
Yekaterina Novikova <yanoviko@ucdavis.edu>
2013-06-04 09:48:17 -0700

    changed css & javascript

--- js/script.js
+++ js/script.js
@@ -7,11 +7,6 @@ $(document).ready(function(){
   var check_balance, amount;
   var sav_balance, overdraft;
 
-	// function get_values() {
-	// 	check_balance = parseInt(check_balance_p.text());
- //    sav_balance = parseInt(sav_balance_p.text());
-	// 	amount = parseInt($("#number").val());
-	// }
 	function deposit(event){
 		event.preventDefault();
 		var check_balance = parseInt(check_balance_p.text());
@@ -21,32 +16,26 @@ $(document).ready(function(){
 	}
 
 	function withdraw(event){
-		console.log('inside the checking wd func')
-
-
 		event.preventDefault();
 	  var check_balance = parseInt(check_balance_p.text());
     var sav_balance = parseInt(sav_balance_p.text());
 		var amount = parseInt($("#number").val());
-	  
 	  var total_money = check_balance + sav_balance;
-	  console.log(amount, total_money);
-
-
 	  if (amount <= total_money) {
 		 	if (amount <= check_balance) {
 	 	  	check_balance_p.text(check_balance - amount);
 	 	  }
 		 	else if (amount > check_balance) {
 		 		overdraft = ((check_balance - amount) - check_balance);
+		 		check_balance_p.text(check_balance - check_balance);
 		 		sav_balance_p.text(sav_balance + overdraft);
 		 	}
-	  } else {
-	  	alert('get a job.');
-	  }
-	
+	  } 
+		else {
+	  	$(".container").css({"background": "rgba(255, 0, 0, 0.7)", "color": "black", });
 
-      // oops!
+$("#warning").css({"display": "block", });
+	  }
 	}
 
 $("#sav_deposit").on("click", s_deposit);
@@ -54,75 +43,32 @@ $("#sav_deposit").on("click", s_deposit);
   var sav_balance_p = $("#sav_balance");
   var sav_balance, amount2;
 
-	function get_values() {
+	function get_values(){
 		sav_balance = parseInt(sav_balance_p.text());
 		amount2 = parseInt($("#number2").val());
 	}
+
 	function s_deposit(event){
-	  event.preventDefault();
-		get_values();
-	  sav_balance_p.text(sav_balance + amount2);
+		event.preventDefault();
+			var sav_balance = parseInt(sav_balance_p.text());
+			var amount2 = parseInt($("#number2").val());
+			sav_balance_p.text(sav_balance + amount2);
 	}
 
 	function s_withdraw(event){
-	  get_values();
  		event.preventDefault()
+    var sav_balance = parseInt(sav_balance_p.text());
+		var amount2 = parseInt($("#number2").val());
 
-		if (amount2 >= sav_balance)
-	 	{
-	 		alert("You have no money!");
-	 	}
-
- 	  else if (amount2 < sav_balance)
+		if (amount2 < sav_balance)
  	  {
  	  	sav_balance_p.text(sav_balance - amount2);
  	  }
+ 	  else if (amount2 >= sav_balance)
+	 	{
+	 		$(".container").css({"background": "rgba(255, 0, 0, 0.7)", "color": "black", });
+
+$("#warning").css({"display": "block", });
+	 	}	  
 	}
 });	
-
-
-
-
-
-
-
-
-
-		// i need to know the original balance
-
-		// i neefd to know how much to wd
-		
-		// i need to see if there is enough money
-		
-		// if not enough
-		  // if there is there enough in savings
-		    // do it
-		  // else
-		    // oops!
-    // bla
-    // da da 
-    // update tge
-		// i need to update the page to show the new balance
-
-
-	
-		
-
-
-// switch(current_balance) {
-// 	case(current_balance > sub_balance):
-// 		blah;
-// 		break;
-// 	case(current_balance < sub_balance < saving_balance):
-// 		blah;
-// 		break;
-// 	case (sub_balance > current_balance)
-// 		blah
-// 		break;
-
-
-// }
-
-
-
-
