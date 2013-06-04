a9412f3ec6edbf5cab8f674b52d9e414e26ad5a6
elizahyde <elizabeth@elizahyde.com>
2013-06-03 20:57:30 -0700

    Add working project

--- js/script.js
+++ js/script.js
@@ -3,26 +3,90 @@ $(document).ready(function(){
 	$("#check_deposit").on("click", deposit);
 	$("#check_withdraw").on("click", withdraw);
   var check_balance_p = $("#check_balance");
+  var sav_balance_p = $("#sav_balance");
   var check_balance, amount;
+  var sav_balance, overdraft;
+
+	// function get_values() {
+	// 	check_balance = parseInt(check_balance_p.text());
+ //    sav_balance = parseInt(sav_balance_p.text());
+	// 	amount = parseInt($("#number").val());
+	// }
+	function deposit(event){
+		event.preventDefault();
+		var check_balance = parseInt(check_balance_p.text());
+    var sav_balance = parseInt(sav_balance_p.text());
+		var amount = parseInt($("#number").val());
+	  check_balance_p.text(check_balance + amount);
+	}
+
+	function withdraw(event){
+		console.log('inside the checking wd func')
+
+
+		event.preventDefault();
+	  var check_balance = parseInt(check_balance_p.text());
+    var sav_balance = parseInt(sav_balance_p.text());
+		var amount = parseInt($("#number").val());
+	  
+	  var total_money = check_balance + sav_balance;
+	  console.log(amount, total_money);
+
+
+	  if (amount <= total_money) {
+		 	if (amount <= check_balance) {
+	 	  	check_balance_p.text(check_balance - amount);
+	 	  }
+		 	else if (amount > check_balance) {
+		 		overdraft = ((check_balance - amount) - check_balance);
+		 		sav_balance_p.text(sav_balance + overdraft);
+		 	}
+	  } else {
+	  	alert('get a job.');
+	  }
+	
+
+      // oops!
+	}
+
+$("#sav_deposit").on("click", s_deposit);
+	$("#sav_withdraw").on("click", s_withdraw);
+  var sav_balance_p = $("#sav_balance");
+  var sav_balance, amount2;
 
 	function get_values() {
-		check_balance = parseInt(check_balance_p.text());
-		amount = parseInt($("#number").val());
+		sav_balance = parseInt(sav_balance_p.text());
+		amount2 = parseInt($("#number2").val());
 	}
-	function deposit(event){
-		get_values();
-	  check_balance_p.text(check_balance + amount);
+	function s_deposit(event){
 	  event.preventDefault();
+		get_values();
+	  sav_balance_p.text(sav_balance + amount2);
 	}
-	function withdraw(event){
+
+	function s_withdraw(event){
 	  get_values();
-    // if things are good
-	 	  check_balance_p.text(check_balance - amount);
-	 	// else
-      // oops!
-	 	event.preventDefault();
+ 		event.preventDefault()
+
+		if (amount2 >= sav_balance)
+	 	{
+	 		alert("You have no money!");
+	 	}
+
+ 	  else if (amount2 < sav_balance)
+ 	  {
+ 	  	sav_balance_p.text(sav_balance - amount2);
+ 	  }
 	}
 });	
+
+
+
+
+
+
+
+
 
 		// i need to know the original balance
 
